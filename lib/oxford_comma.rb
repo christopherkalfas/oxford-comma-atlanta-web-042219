054796b5aba967b3c33a7df6a42dd5a006dd48b5
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-17 16:47:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+  new_string = nil 
+array.join(" , ")
+new_string << 
 end
+
+# that takes an argument array of string elements and converts it into a string using the Oxford comma. For example, the array ["fiddleheads","okra","kohlrabi"] should get converted to the string "fiddleheads, okra, and kohlrabi".
