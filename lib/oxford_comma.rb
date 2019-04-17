555e8bac976c478412c8da2f9f666ad43a988b94
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-17 16:57:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,17 @@
 def oxford_comma(array)
-  new_string = array.join(" , ")
-  return new_string
-end
-
-if array.length = 2 
-  new_string = array.join(" and ")
-elsif array.length >= 3
-  array.each do |element, index|
-    array[index]
+  if array.length == 0
     new_string = array.join(" , ")
+    return new_string
+  elsif array.length = 2 
+    new_string = array.join(" and ")
+  elsif array.length >= 3
+    array.each do |element, index|
+      array[index]
+      last_array_position = array.join(" and #{array[-1]}")
+      array.pop
+      new_string = array.join(" , ")
+    end 
+  end 
+  return new_string + last_array_position
+end 
     
